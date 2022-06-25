48548195090cc76b1003ee1d85e027547bd1312d
kushal <kushalkolar@gmail.com>
2022-06-25 04:20:52 -0400

    basic mcorr widget works and tested, widget layout is not the best

--- mesmerize_viz/widgets.py
+++ mesmerize_viz/widgets.py
@@ -22,7 +22,7 @@ input_readers = [
 
 
 class BatchViewer:
-    def __int__(
+    def __init__(
             self,
             dataframe: pd.DataFrame,
             multi_select: bool = False,
@@ -31,43 +31,60 @@ class BatchViewer:
         self.dataframe: pd.DataFrame = dataframe.reset_index(drop=True)
         self.grid_shape: Tuple[int, int] = None
 
-        self.batch_list_widget_label = widgets.Label(description="batch items:")
+        self.batch_list_widget_label = widgets.Label(value="batch items:")
         if multi_select:
-            self.batch_list_widget: widgets.SelectMultiple = widgets.SelectMultiple()
+            self.batch_list_widget: widgets.SelectMultiple = widgets.SelectMultiple(
+                options=self.dataframe["name"].to_list(),
+                index=0
+            )
         else:
-            self.batch_list_widget: widgets.Select = widgets.Select()
+            self.batch_list_widget: widgets.Select = widgets.Select(
+                options=self.dataframe["name"].to_list(),
+                index=0
+            )
 
         self.batch_list_widget.observe(self.item_selection_changed)
 
         self.uuid_text_widget = widgets.Text(disabled=True, tooltip="UUID of item")
         self.params_text_widget = widgets.Textarea(disabled=True, tooltip="Parameters of item")
 
-        self.outputs_text_widget = widgets.Text(disabled=True, tooltip="Output info of item")
+        self.outputs_text_widget = widgets.Textarea(description="output info", disabled=True, tooltip="Output info of item")
 
         self.accordion = widgets.Accordion(children=[self.outputs_text_widget], titles=('output info'))
 
         self.grid_plot: GridPlot = GridPlot(shape=(1, 3), controllers=np.array([[0, 0, 0]]))
         self.frame_slider = widgets.IntSlider(value=0, min=0, description="frame index:")
+        self.grid_plot.renderer.add_event_handler(self._set_frame_slider_width, "resize")
 
-        self.frame_slider.observe(self.update_grid_plot)
+        self.frame_slider.observe(self.update_frame, "value")
 
         # this should become dynamic later
-        self.current_movies = OrderedDict([
+        self.current_movies: OrderedDict[str, np.ndarray] = OrderedDict([
+            ("input", None),
+            ("mcorr", None),
+        ])
+
+        self.current_graphics: OrderedDict[str, Image] = OrderedDict([
             ("input", None),
             ("mcorr", None),
             ("dsavg", None),
         ])
 
-        self.current_graphics = OrderedDict([
-            ("input", None),
-            ("mcorr", None),
-            ("dsavg", None),
-        ])
+        self.ds_window = 10
+
+        self.item_selection_changed()
+
+    def _set_frame_slider_width(self, *args):
+        w, h = self.grid_plot.renderer.logical_size
+        self.frame_slider.layout = Layout(width=f"{w}px")
 
     def get_selected_index(self) -> int:
         return self.batch_list_widget.index
 
-    def item_selection_changed(self):
+    def item_selection_changed(self, *args):
+        if self.get_selected_index() is None:
+            return
+
         for subplot in self.grid_plot:
             subplot.scene.clear()
 
@@ -75,25 +92,29 @@ class BatchViewer:
         r = self.dataframe.iloc[ix]
 
         self.current_movies["input"] = r.caiman.get_input_movie()
-        mcorr_memmap = r.mcorr.get_output()
-        self.current_movies["mcorr"] = mcorr_memmap
+        self.current_movies["mcorr"] = r.mcorr.get_output()
 
         self.current_graphics["input"] = Image(
             self.current_movies["input"][0],
             cmap="gnuplot2"
         )
 
+        self.frame_slider.max = self.current_movies["input"].shape[0] - 1
+
         self.current_graphics["mcorr"] = Image(
             self.current_movies["mcorr"][0],
             cmap="gnuplot2"
         )
 
-        dsavg = np.nanmean(mcorr_memmap[0:0+5], axis=0)
+        dsavg = self._get_dsavg(frame_index=0)
 
         self.current_graphics["dsavg"] = Image(
             dsavg,
             cmap="gnuplot2"
         )
+
+        for subplot, graphic in zip(self.grid_plot, self.current_graphics.values()):
+            subplot.add_graphic(graphic)
 
         # make graphics, remove any existing from the scene
 
@@ -104,23 +125,38 @@ class BatchViewer:
         self.params_text_widget.value = format_key(r["params"], 0)
         self.outputs_text_widget.value = format_key(r["outputs"], 0)
 
-    def update_grid_plot(self):
-        frame_index = self.frame_slider.value
+    def _get_dsavg(self, frame_index: int) -> np.ndarray:
+        if self.ds_window % 2 == 1:  # make sure it's even
+            self.ds_window += 1
 
-        self.current_graphics
+        start = max(0, (frame_index - int(self.ds_window / 2)))
+        end = min(self.current_movies["mcorr"].shape[0], (frame_index + int(self.ds_window / 2)))
 
-    def get_layout(self):
-        batch_list_uuid_text_params_text = VBox(
-            self.batch_list_widget_label,
-            self.batch_list_widget,
-            self.uuid_text_widget,
-            self.params_text_widget,
-            self.accordion,
+        return np.nanmean(
+            self.current_movies["mcorr"][start:end], axis=0
         )
 
+    def update_frame(self, *args):
+        if self.get_selected_index() is None:
+            return
+
+        ix = self.frame_slider.value
+
+        self.current_graphics["input"].update_data(self.current_movies["input"][ix])
+        self.current_graphics["mcorr"].update_data(self.current_movies["mcorr"][ix])
+        self.current_graphics["dsavg"].update_data(self._get_dsavg(frame_index=ix))
+
+    def get_layout(self):
+        batch_widgets = VBox([self.batch_list_widget_label, self.batch_list_widget])
+
+        uuid_params_output = VBox([self.uuid_text_widget, self.params_text_widget, self.accordion])
+
+        info_widgets = HBox([batch_widgets, uuid_params_output])
+
         return VBox([
-            HBox([batch_list_uuid_text_params_text, self.grid_plot.show()]),
-            self.frame_slider
+            info_widgets,
+            self.frame_slider,
+            self.grid_plot.show()
         ])
 
     def show(self):
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mesmerize-viz',
+    version='0.0',
+    packages=find_packages(),
+    url='https://github.com/kushalkolar/mesmerize-viz',
+    license='GPL v3.0',
+    author='Kushal Kolar',
+    author_email='',
+    description='Mesmerize visualization package using fastplotlib'
+)
