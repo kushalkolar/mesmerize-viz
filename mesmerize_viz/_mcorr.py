17088594e24abf40ed8740999fa67b3291f981bb
kushalkolar <kushalkolar@gmail.com>
2023-03-27 00:30:22 -0400

    ZeroArray allows mixing mean, corr, etc. and mcorr, input in ImageWidget

--- mesmerize_viz/_mcorr.py
+++ mesmerize_viz/_mcorr.py
@@ -8,7 +8,7 @@ from mesmerize_core import MCorrExtensions
 from mesmerize_core.caiman_extensions._utils import validate as validate_algo
 from fastplotlib import ImageWidget
 
-from ._utils import validate_data_options
+from ._utils import validate_data_options, ZeroArray
 
 
 projs = [
@@ -16,6 +16,10 @@ projs = [
     "max",
     "std",
 ]
+
+
+# these are directly manged by the image widget since they are [t, x, y]
+standard_mappings = ["input", "mcorr"]
 
 
 @pd.api.extensions.register_series_accessor("mcorr")
@@ -31,6 +35,11 @@ class MCorrExtensionsViz(MCorrExtensions):
         }
         return m
 
+    @property
+    def _zero_array(self):
+        mcorr = self.get_output()
+        return ZeroArray(ndim=mcorr.ndim, n_frames=mcorr.shape[0])
+
     @validate_algo("mcorr")
     @validate_data_options()
     def viz(
@@ -44,15 +53,23 @@ class MCorrExtensionsViz(MCorrExtensions):
 
         Parameters
         ----------
-        data
-        input_movie_kwargs
-        image_widget_kwargs
+        data: list of str, default ["input", "mcorr"]
+            list of data to plot, can also be a list of lists.
+
+        input_movie_kwargs: dict, optional
+            kwargs passed to get_input_movie()
+
+        image_widget_kwargs: dict, optional
+            kwargs passed to ImageWidget
 
         Returns
         -------
-
+        ImageWidget
+            fastplotlib.ImageWidget visualization
         """
+
         if data is None:
+            # default viz
             data = ["input", "mcorr"]
 
         if input_movie_kwargs is None:
@@ -62,16 +79,25 @@ class MCorrExtensionsViz(MCorrExtensions):
             image_widget_kwargs = dict()
 
         data_arrays = list()
+        # data arrays directly passed to image widget
+        data_arrays_iw = list()
 
         for d in data:
-            func = self._data_mapping[d]
+            if d in standard_mappings:
+                func = self._data_mapping[d]
 
-            if d == "input":
-                a = func(**input_movie_kwargs)
+                if d == "input":
+                    a = func(**input_movie_kwargs)
+                else:
+                    a = func()
+
+                data_arrays_iw.append(a)
+
             else:
-                a = func()
-
-            data_arrays.append(a)
+                # make a placeholder array to keep imagewidget happy
+                # hacky but this is the best way for now
+                zero_array = self._zero_array
+                data_arrays_iw.append(zero_array)
 
         # default kwargs unless user has specified
         default_iw_kwargs = {
@@ -86,10 +112,20 @@ class MCorrExtensionsViz(MCorrExtensions):
         }
 
         iw = ImageWidget(
-            data=data_arrays,
+            data=data_arrays_iw,
             names=data,
             **image_widget_kwargs
         )
+
+        for a, n in zip(data_arrays_iw, data):
+            if isinstance(a, ZeroArray):
+                # rename the existing graphic
+                iw.plot[n].graphics[0].name = "zero-array-ignore"
+                # get the real data
+                func = self._data_mapping[n]
+                real_data = func()
+                # create graphic with the real data, this will not be managed by ImageWidget
+                iw.plot[n].add_image(real_data, name="img", cmap="gnuplot2")
 
         iw.show()
         return iw
--- mesmerize_viz/_utils.py
+++ mesmerize_viz/_utils.py
@@ -2,6 +2,9 @@ from itertools import chain
 from functools import wraps
 from typing import *
 
+import numpy as np
+from mesmerize_core.arrays._base import LazyArray
+
 
 def validate_data_options():
     def dec(func):
@@ -33,3 +36,38 @@ def validate_data_options():
         return wrapper
 
     return dec
+
+
+class ZeroArray(LazyArray):
+    """
+    This array is used as placeholders to allow mixing data of different ndims in the ImageWidget.
+    For example this allows having mean, max etc. projections in the same ImageWidget as the
+    input or mcorr movie. It also allows having LineStacks or Heatmap in the same ImageWidget.
+    """
+    def __init__(self, ndim, n_frames):
+        self._shape = [1] * ndim
+        self._shape[0] = n_frames
+        self.rval = np.zeros(shape=self.shape, dtype=np.int8)
+
+    @property
+    def dtype(self) -> str:
+        return "int8"
+
+    @property
+    def shape(self) -> Tuple[int, int, int]:
+        return tuple(self._shape)
+
+    @property
+    def n_frames(self) -> int:
+        return self.shape[0]
+
+    @property
+    def min(self) -> float:
+        return 0.0
+
+    @property
+    def max(self) -> float:
+        return 0.0
+
+    def _compute_at_indices(self, indices: Union[int, slice]) -> np.ndarray:
+        return np.zeros(shape=(1, 1, 1), dtype=np.int8)
